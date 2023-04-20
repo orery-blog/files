7757c7b66c507192991dab96caed679856d39346
orery-blog <129051459+orery-blog@users.noreply.github.com>
2023-04-20 20:06:11 +0900

    Create tubakioil.js

---
+++ kattene/tubakioil.js
@@ -0,0 +1,35 @@
+const div=`
+<div class="kattene">
+    <div class="kattene__imgpart"><a
+            href="//af.moshimo.com/af/c/click?a_id=1701180&p_id=170&pc_id=185&pl_id=4062&url=https%3A%2F%2Fwww.amazon.co.jp%2Fs%3Fk%3D%25E6%25A4%25BF%25E6%25B2%25B9"
+            referrerpolicy="no-referrer-when-downgrade" rel="nofollow" target="_blank"><img alt="椿油"
+                src="https://images-fe.ssl-images-amazon.com/images/I/415Z30cZyXL._SL160_.jpg" width="160px"
+                height="160px" loading="lazy"></a></div>
+    <div class="kattene__infopart">
+        <div class="kattene__title"><a
+                href="//af.moshimo.com/af/c/click?a_id=1701180&p_id=170&pc_id=185&pl_id=4062&url=https%3A%2F%2Fwww.amazon.co.jp%2Fs%3Fk%3D%25E6%25A4%25BF%25E6%25B2%25B9"
+                referrerpolicy="no-referrer-when-downgrade" rel="nofollow" target="_blank">椿油
+            </a></div>
+        <div class="kattene__description">
+            精製した生搾りのオーガニック椿油100%です．保湿力・浸透性に優れ，パサつき，フケ，カユミ，枝毛，切れ毛を防ぎ，パーマやヘアカラーで傷んだ髪のケアに最適．髪だけでなく顔や肌などの全身用の美容オイルとしてもおすすめです．
+            <img src="//i.moshimo.com/af/i/impression?a_id=1701180&p_id=170&pc_id=185&pl_id=4062" width="1" height="1"
+                style="border:none;" alt="">
+            <img src="//i.moshimo.com/af/i/impression?a_id=1674572&p_id=54&pc_id=54&pl_id=616" width="1" height="1"
+                style="border:none;" alt="">
+                <img src="//i.moshimo.com/af/i/impression?a_id=1701181&p_id=1225&pc_id=1925&pl_id=18502" width="1" height="1" style="border:none;" alt="">
+        </div>
+        <div class="kattene__btns __three">
+            <div><a class="kattene__btn __orange"
+                    href="//af.moshimo.com/af/c/click?a_id=1701180&p_id=170&pc_id=185&pl_id=4062&url=https%3A%2F%2Fwww.amazon.co.jp%2Fs%3Fk%3D%25E6%25A4%25BF%25E6%25B2%25B9"
+                    referrerpolicy="no-referrer-when-downgrade" rel="nofollow" target="_blank">Amazon</a></div>
+            <div><a class="kattene__btn __red"
+                    href="//af.moshimo.com/af/c/click?a_id=1674572&p_id=54&pc_id=54&pl_id=616&url=https%3A%2F%2Fsearch.rakuten.co.jp%2Fsearch%2Fmall%2F%25E6%25A4%25BF%25E6%25B2%25B9"
+                    referrerpolicy="no-referrer-when-downgrade" rel="nofollow" target="_blank">楽天</a></div>
+            <div><a class="kattene__btn __blue"
+                    href="//af.moshimo.com/af/c/click?a_id=1701181&p_id=1225&pc_id=1925&pl_id=18502&url=https%3A%2F%2Fshopping.yahoo.co.jp%2Fsearch%3Fp%3D%25E6%25A4%25BF%25E6%25B2%25B9"
+                    referrerpolicy="no-referrer-when-downgrade" rel="nofollow" target="_blank">Yahoo!</a></div>
+        </div>
+    </div>
+</div>
+`
+document.currentScript.insertAdjacentHTML('beforebegin', div)
